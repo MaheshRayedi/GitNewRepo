505fbad195d8144e4f4a2e938e3af81203a4a1c1
MaheshRayedi <rayedimahesh123@gmail.com>
2023-05-06 20:35:37 +0530

    Added space in third line demo class

--- trainingautomation/src/icici/car/loans/Demo.java
+++ trainingautomation/src/icici/car/loans/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("i am new created");
 		System.out.println("Second line");
-		System.out.println("Third line");
+		System.out.println("Third  line");
 	}
 
 }
