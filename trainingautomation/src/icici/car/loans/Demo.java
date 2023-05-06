16b732327c59132dad8b7806120f0dcc82f8bfd0
MaheshRayedi <rayedimahesh123@gmail.com>
2023-05-06 20:18:05 +0530

    Added Third line in Demo class

--- trainingautomation/src/icici/car/loans/Demo.java
+++ trainingautomation/src/icici/car/loans/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("i am new created");
 		System.out.println("Second line");
+		System.out.println("Third line");
 	}
 
 }
